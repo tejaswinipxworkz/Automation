7b41a404ee697af296f8281449f77656e7358a57
tejaswinipxworkz <89890958+tejaswinipxworkz@users.noreply.github.com>
2023-12-28 12:12:54 +0530

    Add files via upload

---
+++ Day11/MyInterfaceDemo.java
@@ -0,0 +1,23 @@
+package Day11;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyInterfaceDemo {
+@Test
+void m1() {
+	Assert.assertEquals(1, 1);
+}
+@Test
+void m2() {
+	Assert.assertEquals(1, 2);
+}
+@Test
+void m3() {
+	Assert.assertEquals(1, 1);
+}
+
+
+
+
+}
